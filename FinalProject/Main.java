8e9c2a3153d462707b3ed185896481237cc9e5c6
George Leyva <leyvageorge@hotmail.com>
2026-03-02 06:37:03 -0800

    second commit

---
+++ FinalProject/Main.java
@@ -0,0 +1,8 @@
+package FinalProject;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create an instance of the game and start it
+        // Automobile coupe = new Automobile();
+    }
+}
